180fb1e8674aa7403bfc4d6c38a8fcd6c936928e
csivori <43187099+csivori@users.noreply.github.com>
2022-10-13 18:23:28 -0300

    Update publicaciones.js

--- datos/publicaciones.js
+++ datos/publicaciones.js
@@ -15,5 +15,11 @@ const publicaciones = [{
                     "titulo": "Comunicado de la Asociación Civil Los Manantiales",
                     "texto": "Sobre la reunión en comisión del Consejo de Gestión de la Reserva con el Concejo Deliberante el Lunes 5 de Abril. Tema: La disminución de la tasa y presupuesto de la Reserva Hídrica Natural Municipal Los Manantiales",
                     "url": "https://onglosmanantiales.wordpress.com/2021/10/28/comunicado-de-la-asociacion-civil-los-manantiales-por-el-proyecto-de-modificacion-de-la-ordenanza-de-usos-de-suelo-de-rio-ceballos-n-2651-19/"
+                  },
+                  {
+                    "img":"../img/logoTexto.png",
+                    "titulo":"TITULO",
+                    "texto":"TEXTO",
+                    "url":"https://github.com/maernoud/CodoACodo-front"
                   }
                 ];
