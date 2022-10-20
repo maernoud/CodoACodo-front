0f04865de785ca3a8074b84dcddfe01b33f25d08
Manuel Genoud <maernoud@gmail.com>
2022-10-20 17:19:50 -0300

    vuelta a atras a commit mio de esta mañana

--- js/mostrar_pub.js
+++ js/mostrar_pub.js
@@ -1,7 +1,7 @@
 const getCard = (pub) => {
   let sHTML=`<div class="column">
-            <div class="card">
-              <img class="card-img-top" src=${pub.img} alt="Card image cap">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src=${pub.img} alt="Card image cap" style="width: 18rem;">
               <div class="card-body">
                 <h5 class="card-title">${pub.titulo}</h5>
                 <p class="card-text">${pub.texto}</p>
@@ -9,8 +9,6 @@ const getCard = (pub) => {
               </div>
             </div>
           </div>`;
-  //<div class="card" style="width: 18rem;">
-  //  <img class="card-img-top" src=${pub.img} alt="Card image cap" style="width: 18rem;">
   return sHTML;
 }
 
